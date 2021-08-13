1897551bca25d07dd776e68a1ded789d6556041e
Natalie-Cardot <nataliecardot@gmail.com>
2021-08-12 21:15:36 -0700

    Add comments

--- src/context.js
+++ src/context.js
@@ -4,13 +4,23 @@ import reducer from './reducer';
 const url = 'https://course-api.com/react-useReducer-cart-project';
 const AppContext = React.createContext();
 
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
+
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems);
+  // only update state once we call dispatch, once proper action is passed
+  // action is object passed to dispatch(), which object must have property by name of type (which is usually uppercase but not necessary), e.g.: dispatch({type: 'TESTING'})
+  // once action is dispatched, need to handle it in reducer
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
       }}
     >
       {children}
---
+++ src/reducer.js
@@ -0,0 +1,8 @@
+// once action is dispatched, need to handle it in reducer
+// state is current state before update, action is what we're trying to do (the action of a specific type dispatched by dispatch() function)
+// must always return new state
+const reducer = (state, action) => {
+  return state;
+};
+
+export default reducer;
