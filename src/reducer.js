6d7acaef60adf00d83b3b5d68b1d628d467cde8d
nataliecardot <nataliecardot@gmail.com>
2021-09-22 21:52:06 -0700

    Add comment

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,4 @@
+// a reducer is a function that determines changes to an application's state
 // once action is dispatched, need to handle it in reducer
 // state is current state before update, action is what we're trying to do (the action of a specific type dispatched by dispatch() function)
 // must always return updated state (...state, newState)
